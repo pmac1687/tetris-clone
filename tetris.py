83852122114572fb26d472d70b096fdc979104c1
pmac1687 <58713706+pmac1687@users.noreply.github.com>
2020-04-16 20:11:25 -0700

    Add files via upload
    last edition before abandoning the single image termino for building from four images.moved boundry collision from tetri to update in termino.added method in termonos to stop termino creation
    
    added down,left,right key functionality

--- tetris.py
+++ tetris.py
@@ -3,29 +3,41 @@ import time
 import terminoes
 
 green_l = pygame.image.load('green_L.png')
+T2_img_2 = pygame.image.load('blue_I_vert.png')
 
-def check_collide():
-    for i in termino_group:
+"""def check_collide():
+    for i in all_sprites:
         if (i.rect.collidelist(boundries)) != -1:
             termino.speed = 0
             termino.rect.y = 500
             termino.spin = False
-            termino_group = termino_group.remove(i)
-            create_new()
+            create_new()"""
 
 def create_new():
-    termino = terminoes.Termino_2(55,25)
-    all_sprites.add(termino)
+    for i in all_sprites:
+        if i.speed == 0:
+            #make termino condition so it doesnt call if statement again
+            i.speed = 5
+            i.move = False
+            for b in all_sprites:
+                if b not in boundries:
+                    boundries.append(b)
+                    new = b
+                    all_sprites_2.add(new)
+                    all_sprites.remove(b)
+                    termino = terminoes.Termino_1(59,25,T2_img_2,boundries)
+                    termino_group.append(termino)
+                    all_sprites.add(termino)
 
 def populate_boundry():
     boundry_lst = []
     x= 0
     y= 550
-    for i in range(10):
+    for i in range(40):
         boundry_section = terminoes.Boundry(x,y)
         boundries.append(boundry_section)
-        x += 20
-    all_sprites.add(boundry_lst)
+        x += 10
+    all_sprites_boundry.add(boundry_lst)
     #boundries.add(boundry_lst)
         
 
@@ -33,22 +45,29 @@ pygame.init()
 
 screen = pygame.display.set_mode((400, 600))
 pygame.display.set_caption('title')
-
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
+all_sprites_2 = pygame.sprite.Group()
+all_sprites_boundry = pygame.sprite.Group()
 termino_group = []
 boundries = []
 populate_boundry()
-termino = terminoes.Termino_1(25,25, green_l )
+termino_1 = terminoes.Termino_1(925,625, green_l,boundries )
+termino = terminoes.Termino_1(25,25, green_l,boundries )
+termino_group.append(termino)
 termino_group.append(termino)
 all_sprites.add(termino)
+all_sprites_2.add(termino_1)
 
 running = True
 
 while running:
     clock.tick(60)
     all_sprites.update()
-    check_collide()
+    all_sprites_2.update()
+    create_new()
+    
+    #check_collide()
     
     for event in pygame.event.get():
 
@@ -58,16 +77,50 @@ while running:
             exit()
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-               pass
+
+            if event.key == pygame.K_RIGHT:
+               for e in all_sprites:
+                   if e.rect.x > 0 and e.rect.x < 380:
+                       e.speed_vert += 5
+            if event.key == pygame.K_LEFT:
+               for e in all_sprites:
+                   if e.rect.x > 0 and e.rect.x < 380:
+                       e.speed_vert -= 5
             if event.key == pygame.K_DOWN:
-                pass
+                for i in all_sprites:
+                   if i.move == True:
+                       i.speed = 9
+        
+            if event.key == pygame.K_UP:
+               for i in all_sprites:
+                   if i.move == True:
+                       i.image = pygame.transform.rotate(i.image, i.angle)
+            if event.key == pygame.K_SPACE:
+               for i in all_sprites:
+                   if i.move == True:
+                       i.image = pygame.transform.rotate(i.image, -i.angle)
+
+
         if event.type == pygame.KEYUP:
-            pass
+
+            if event.key == pygame.K_DOWN:
+                for i in all_sprites:
+                   if i.move == True:
+                       i.speed = 2
+            if event.key == pygame.K_RIGHT:
+               for e in all_sprites:
+                   if e.rect.x > 0 and e.rect.x < 380:
+                       e.speed_vert -= 5
+            if event.key == pygame.K_LEFT:
+               for e in all_sprites:
+                   if e.rect.x > 0 and e.rect.x < 380:
+                       e.speed_vert += 5 
             
 
     screen.fill((255,0,0))
     all_sprites.draw(screen)
+    all_sprites_2.draw(screen)
+    all_sprites_boundry.draw(screen)
 
     #screen.blit
     #all sprite.draw
