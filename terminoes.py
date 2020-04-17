88e98d51cdd0bc4effdf930e1c51f88039c8494d
pmac1687 <58713706+pmac1687@users.noreply.github.com>
2020-04-17 06:56:12 -0700

    Add files via upload
    4-image termino, bug after 4th termino, sprite slows way down

--- terminoes.py
+++ terminoes.py
@@ -1,20 +1,23 @@
 import pygame
 
 
+
 T2_img_1 =  pygame.image.load('blue_I.png')
 T2_img_2 = pygame.image.load('blue_I_vert.png')
 
 
 class Termino_1(pygame.sprite.Sprite):
-    def __init__(self, x, y,img):
+    def __init__(self, x, y,img,boundries):
         pygame.sprite.Sprite.__init__(self)
+        self.boundries = boundries
         self.image = img
-        self.image =  pygame.transform.scale(self.image , (50,50))
+        self.image =  pygame.transform.scale(self.image , (20,20))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.move = True
         self.speed = 2
+        self.speed_vert = 0
         self.spin = True
         self.spin_fps = 4
         self.angle = 90
@@ -30,9 +33,24 @@ class Termino_1(pygame.sprite.Sprite):
                 self.angle += 90
             else:
                 self.spin_fps += 1"""
+        if self.rect.x < 0 or self.rect.x > 380:
+            self.speed_vert = 0
         if self.move == True:
             self.rect.y += self.speed
+            self.rect.x += self.speed_vert
+            if (self.rect.collidelist(self.boundries)) != -1  :
+                self.speed = 0
+                self.rect.y -= 1
+                self.spin = False
+                self.move = False
+        """ if len(self.all_sprites) > 0:
+                if(self.rect.collidelist(self.all_sprites)) != -1:
+                    self.speed = 0
+                    self.rect.y = 500
+                    self.spin = False
+                    self.move = False"""
 
+    
 class Boundry(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -42,41 +60,9 @@ class Boundry(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
 
-class Termino_2(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = T2_img_2
-        self.image =  pygame.transform.scale(self.image , (75,25))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.move = True
-        self.speed = 2
-        self.spin = True
-        self.spin_fps = 3
-        self.angle = 90
         
 
-    def update(self):
-        self.get_rotate()
-
-
-    def get_rotate(self):
-        if self.spin == True:
-            if self.spin_fps == 4:
-                if self.image == T2_img_1 :
-                    self.image = T2_img_2
-                    self.image =  pygame.transform.scale(self.image , (75,25))
-                elif self.image == T2_img_2:
-                    self.image = T2_img_1 
-                    self.image =  pygame.transform.scale(self.image , (75,25))
-                    
-                self.spin_fps = 0
-            else:
-                self.spin_fps += 1
-        if self.move == True:
-            self.rect.y += self.speed
-
+   
 
 
 
